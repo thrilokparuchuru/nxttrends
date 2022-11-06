1f0b0b374377d78d7923aa31c6195b0bc6d26d6e
trilokparuchuru <trilokparuchuru2@gmail.com>
2022-11-06 11:28:49 +0000

    first commit

---
+++ src/components/LoginForm/index.js
@@ -0,0 +1,77 @@
+/* eslint-disable prettier/prettier */
+// Write your JS code here
+
+import {Component} from 'react'
+import './index.css'
+
+class LoginForm extends Component {
+  state = {userName: '', password: ''}
+
+  username = event => {
+    this.setState({userName: event.target.value})
+  }
+
+  password = event => {
+    this.setState({password: event.target.value})
+  }
+
+  onSubmitForm = async event => {
+    event.preventDefault()
+    const {userName, password} = this.state
+    const bodyData = {userName, password}
+
+    const url = 'https://apis.ccbp.in/login'
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(bodyData),
+    }
+
+    const response = await fetch(url, options)
+    // const data = await response.json()
+    console.log(response)
+  }
+
+  render() {
+    return (
+      <div className="login-route-container">
+        <img
+          className="login-route-image"
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-login-img.png"
+          alt="website login"
+        />
+        <form onSubmit={this.onSubmitForm}>
+          <img
+            className="login-route-form-logo"
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+            alt="website logo"
+          />
+          <div className="username-container">
+            <label htmlFor="UserName">UserName</label> <br />
+            <input
+              className="form-input"
+              type="input"
+              id="UserName"
+              placeholder=" Username"
+              onChange={this.username}
+            />
+          </div>
+          <div className="password-container">
+            <label htmlFor="Password">Password</label> <br />
+            <input
+              className="form-input"
+              type="password"
+              id="Password"
+              placeholder=" Password"
+              onChange={this.password}
+            />
+          </div>
+          <button type="submit" className="login-button">
+            Login
+          </button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default LoginForm
